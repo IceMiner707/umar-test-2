87a6e8ed3b92baa5b9552beb9012fe9dc657bd28
IceMiner707 <umlicraft@gmail.com>
2023-10-17 17:24:26 +0800

    menambahkan paragraph lorem50

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 </head>
 <body>
     <h1>HEllow World</h1>
-    
+    <p>Lorem ipsum dolor sit
+         amet consectetur, adipisicing elit. Dolore, at, molestias beatae consequatur dolorem culpa ipsam quibusdam doloribus ipsa libero deleniti consectetur ratione totam nisi, voluptas laboriosam magnam magni sed iure error. Optio animi dolores minima. Cupiditate modi consectetur aperiam suscipit impedit at nesciunt sequi. Accusantium adipisci minima quos saepe.
+
+    </p>
 </body>
 </html>
